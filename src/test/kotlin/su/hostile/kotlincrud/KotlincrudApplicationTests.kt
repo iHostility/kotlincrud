a9540c0d04c723edd8ff2bbcc81cf68a68ae65ec
Denis Voloshin <dvoloshin@licard.com>
2022-09-27 00:22:00 +0300

    Initial commit

---
+++ src/test/kotlin/su/hostile/kotlincrud/KotlincrudApplicationTests.kt
@@ -0,0 +1,13 @@
+package su.hostile.kotlincrud
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlincrudApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
